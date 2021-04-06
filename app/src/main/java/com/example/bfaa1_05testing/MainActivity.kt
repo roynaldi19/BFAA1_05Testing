9c693b0af58dbb317d5968898703807651b66f06
roynaldi19 <roynaldi19@gmail.com>
2021-04-06 21:42:32 +0700

    comit april

--- app/src/main/java/com/example/bfaa1_05testing/MainActivity.kt
+++ app/src/main/java/com/example/bfaa1_05testing/MainActivity.kt
@@ -2,14 +2,13 @@ package com.example.bfaa1_05testing
 
 import android.os.Bundle
 import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
+import com.bumptech.glide.Glide
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
+class MainActivity : AppCompatActivity() {
 
     private lateinit var btnSetValue: Button
     private lateinit var tvText: TextView
@@ -17,25 +16,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private var names = ArrayList<String>()
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        btnSetValue = findViewById(R.id.btn_set_value)
         tvText = findViewById(R.id.tv_text)
-
-        btnSetValue.setOnClickListener(this)
-
-        names.add("Narenda Wicaksono")
-        names.add("Kevin")
-        names.add("Yoza")
-
-        imgPreview = findViewById(R.id.img_preview)
-        imgPreview.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.fronalpstock_big))
-    }
-
-    override fun onClick(view: View) {
-        if (view.id == R.id.btn_set_value) {
+        btnSetValue = findViewById(R.id.btn_set_value)
+        btnSetValue.setOnClickListener {
             Log.d("MainActivity", names.toString())
             val name = StringBuilder()
             for (i in 0..2) {
@@ -43,5 +31,14 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
             }
             tvText.text = name.toString()
         }
+
+        names.add("Roy")
+        names.add("Rio")
+        names.add("Riyo")
+
+        imgPreview = findViewById(R.id.img_preview)
+        Glide.with(this).load(R.drawable.fronalpstock_big).into((imgPreview))
     }
+
+
 }
